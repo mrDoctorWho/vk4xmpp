9d005591ec913fa74da3eeb05e5bdada309c611e
mrDoctorWho <mrdoctorwho@gmail.com>
2013-09-19 10:37:14 +0700

    handlers is now placed in directory

---
+++ handlers/Message.py
@@ -0,0 +1,75 @@
+# coding: utf
+# This file is a part of VK4XMPP transport
+# © simpleApps, 2013.
+
+def msgRecieved(msg, jidFrom, jidTo):
+	if msg.getTag("request"):
+		answer = xmpp.Message(jidFrom)
+		tag = answer.setTag("received", namespace = "urn:xmpp:receipts")
+		tag.setAttr("id", msg.getID())
+		answer.setFrom(jidTo)
+		answer.setID(msg.getID())
+		return answer
+
+def msgHandler(cl, msg):
+	mType = msg.getType()
+	jidFrom = msg.getFrom()
+	jidFromStr = jidFrom.getStripped()
+	if jidFromStr in Transport and mType == "chat":
+		Class = Transport[jidFromStr]
+		jidTo = msg.getTo()
+		body = msg.getBody()
+		if body:
+			answer = None
+			if jidTo == TransportID:
+				raw = body.split(None, 1)
+				if len(raw) > 1:
+					text, args = raw
+					args = args.strip()
+					if text == "!captcha" and args:
+						captchaAccept(cl, args, jidTo, jidFromStr)
+						answer = msgRecieved(msg, jidFrom, jidTo)
+					elif text == "!eval" and args and jidFromStr == evalJID:
+						try:
+							msg = unicode(eval(args))
+						except Exception, ErrorMsg:
+							try:
+								ErrorMsg = str(ErrorMsg)
+							except:
+								ErrorMsg = unicode(ErrorMsg)
+							msg = 'Error! %s' % ErrorMsg
+						msgSend(cl, jidFromStr, msg, jidTo)
+			else:
+				uID = jidTo.getNode()
+				vkMessage = Class.msg(body, uID)
+				if vkMessage:
+					answer = msgRecieved(msg, jidFrom, jidTo)
+			if answer:
+				Sender(cl, answer)
+		else:
+			raise xmpp.NodeProcessed()
+
+def captchaAccept(cl, args, jidTo, jidFromStr):
+	if args:
+		answer = None
+		Class = Transport[jidFromStr]
+		if Class.vk.engine.captcha:
+			logger.debug("user %s called captcha challenge" % jidFromStr)
+			Class.vk.engine.captcha["key"] = args
+			retry = False
+			try:
+				logger.debug("retrying for user %s" % jidFromStr)
+				retry = Class.vk.engine.retry()
+			except api.captchaNeeded:
+				logger.error("retry for user %s failed!" % jidFromStr)
+				Class.vk.captchaChallenge()
+			if retry:
+				logger.debug("retry for user %s OK" % jidFromStr)
+				answer = _("Captcha valid.")
+				Class.tryAgain()
+			else:
+				answer = _("Captcha invalid.")
+		else:
+			answer = _("Not now. Ok?")
+		if answer:
+			msgSend(cl, jidFromStr, answer, jidTo)
---
+++ handlers/Presence.py
@@ -0,0 +1,49 @@
+# coding: utf
+# This file is a part of VK4XMPP transport
+# © simpleApps, 2013.
+
+def prsHandler(cl, prs):
+	pType = prs.getType()
+	jidFrom = prs.getFrom()
+	jidTo = prs.getTo()
+	jidFromStr = jidFrom.getStripped()
+	jidToStr = jidTo.getStripped()
+	if jidFromStr in Transport:
+		Class = Transport[jidFromStr]
+		Resource = jidFrom.getResource()
+		if pType in ("available", "probe", None):
+			if Resource not in Class.resources:
+				logger.debug("%s from user %s, will send sendInitPresence" % (pType, jidFromStr))
+				Class.resources.append(Resource)
+				if Class.lastStatus == "unavailable" and len(Class.resources) == 1:
+					if not Class.vk.Online:
+						Class.vk.Online = True
+						Class.vk.onlineMe()
+				Class.sendInitPresence()
+
+		elif pType == "unavailable":
+			if Resource in Class.resources:
+				Class.resources.remove(Resource)
+				if not Class.resources:
+					Sender(cl, xmpp.Presence(jidFrom, "unavailable", frm = TransportID))
+					Class.vk.disconnect()
+				else:
+					Class.sendOutPresence(jidFrom)
+	
+		elif pType == "error":
+			eCode = prs.getErrorCode()
+			if eCode == "404":
+				Class.vk.disconnect()
+
+		elif pType == "subscribe":
+			if jidToStr == TransportID:
+				Sender(cl, xmpp.Presence(jidFromStr, "subscribed", frm = TransportID))
+				Sender(cl, xmpp.Presence(jidFrom, frm = TransportID))
+			else:
+				Sender(cl, xmpp.Presence(jidFromStr, "subscribed", frm = jidTo))
+				if Class.friends:
+					id = vk2xmpp(jidToStr)
+					if id in Class.friends:
+						if Class.friends[id]["online"]:
+							Sender(cl, xmpp.Presence(jidFrom, frm = jidTo))
+		Class.lastStatus = pType
