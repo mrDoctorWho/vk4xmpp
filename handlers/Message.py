864320619b11bba397cbfb8f4409913871293e4f
mrDoctorWho <mrdoctorwho@gmail.com>
2013-09-23 00:55:13 +0700

    captcha fix

--- handlers/Message.py
+++ handlers/Message.py
@@ -66,7 +66,7 @@ def captchaAccept(cl, args, jidTo, jidFromStr):
 			if retry:
 				logger.debug("retry for user %s OK" % jidFromStr)
 				answer = _("Captcha valid.")
-				Presence = xmpp.Protocol.Presence(self.jidFrom, frm = TransportID)
+				Presence = xmpp.protocol.Presence(self.jidFrom, frm = TransportID)
 				Presence.setStatus("") # is it needed?
 				Presence.setShow("available")
 				Sender(Presence)
