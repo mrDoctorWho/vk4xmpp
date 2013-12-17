439d47c51633b11e3c385ab6270e9065fd4cf3a6
ernado <ernado@ya.ru>
2013-12-17 08:57:11 +0400

    reverted changes, re-refactored

--- library/vkApi.py
+++ library/vkApi.py
@@ -3,21 +3,43 @@
 # Warning: Code in this module is ugly,
 # but we can't do better.
 
-import re, time
-import ssl, urllib, urllib2, cookielib, webtools
+import ssl, urllib, urllib2, cookielib, webtools, time, logging
 
 try:
-	import simplejson as json # We hope you're read wiki.
+	# TODO: use ujson for speed
+	import simplejson as json
 except ImportError:
 	import json
 
-class RequestProcessor(object):
+logger = logging.getLogger("vk4xmpp")
 
-	headers = { "User-agent": "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:21.0)"\
-									" Gecko/20130309 Firefox/21.0",
-		   	   	"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-				"Accept-Language": "ru-RU, utf-8"
-			  }
+
+def attemptTo(f):
+	"""
+	Tries to execute function for 5 times, silences (urllib2.URLError, ssl.SSLError)
+	"""
+	def wrapper(*args, **kwargs):
+		maxRetries = 5
+		retries = 0
+		errors = (urllib2.URLError, ssl.SSLError)
+
+		while maxRetries > retries:
+			try:
+				return f(*args, **kwargs)
+			except errors as e:
+				logger.debug('Error %s occured on safeExecute' % e)
+				retries += 1
+		return {}
+
+	return wrapper
+
+
+class RequestProcessor(object):
+	headers = {"User-agent": "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:21.0)"
+				" Gecko/20130309 Firefox/21.0",
+				"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+				"Accept-Language": "ru-RU, utf-8"}
+
 	def __init__(self):
 		self.cookieJar = cookielib.CookieJar()
 		self.cookieProcessor = urllib2.HTTPCookieProcessor(self.cookieJar)
@@ -28,7 +50,7 @@ class RequestProcessor(object):
 			if cookie.name == name:
 				return cookie.value
 
-	def request(self, url, data = None, headers = {}):
+	def request(self, url, data=None, headers=None):
 		if not headers:
 			headers = self.headers
 		if data:
@@ -36,50 +58,33 @@ class RequestProcessor(object):
 		request = urllib2.Request(url, data, headers)
 		return request
 
-	def open(self, request, timeout = 3):
-		response = self.safeExecution(self.Opener.open, (request, None, timeout))
-		return response
+	def open(self, request, timeout=3):
+		return attemptTo(self.open)(request, None, timeout)
 
-	def safeExecution(self, func, args = (), retryCount = 0):
-		try:
-			if retryCount > 5: 	## We're hope that it will be never called
-				raise RuntimeError
-			result = func(*args)
-		except (urllib2.URLError, ssl.SSLError):
-			retryCount += 1
-			result = self.safeExecution(func, args, retryCount)
-		except RuntimeError: ## We're so sad :( for this fact, really, very sad
-			result = {}
-		return result
-
-	def post(self, url, data = {}, retryCount = 0):
+	@attemptTo
+	def post(self, url, data=None):
 		body = {}
-		request = self.request(url, data)
-		response = self.safeExecution(self.open, (request,))
-		try:
-			if response:
-				body = response.read()		## we can't use safeExecution because .read() method can be used once
-			if retryCount > 2:
-				raise RuntimeError
-		except (urllib2.URLError, ssl.SSLError):
-			retryCount += 1
-			return self.post(url, data, retryCount)
-		except RuntimeError:
-			body = {}
-		return (body, response)
 
-	def get(self, url, data = {}):
+		if data is None:
+			data = {}
+
+		request = self.request(url, data)
+		response = self.open(request)
+
+		return body, response
+
+	def get(self, url, data=None):
 		if data:
-			url = url + "/?%s" % urllib.urlencode(data)
+			url += "/?%s" % urllib.urlencode(data)
 		request = self.request(url)
-		response = self.safeExecution(self.open, (request,)) # Why again?
+		response = attemptTo(self.open)(request)
 		body = response.read()
-		return (body, response)
+		return body, response
 
 
 class APIBinding:
-	def __init__(self, number, password = None, token = None, app_id = 3789129, 
-				 scope = 69634):
+	def __init__(self, number, password=None, token=None, app_id=3789129,
+	scope=69634):
 		self.password = password
 		self.number = number
 
@@ -97,11 +102,10 @@ class APIBinding:
 
 	def loginByPassword(self):
 		url = "https://login.vk.com/"
-		values = { "act": "login",
-				   "utf8": "1", # check if it needed
-				   "email": self.number,
-				   "pass": self.password 
-				  }
+		values = {"act": "login",
+		"utf8": "1",    # check if it needed
+		"email": self.number,
+		"pass": self.password}
 
 		post = self.RIP.post(url, values)
 		body, response = post
@@ -120,19 +124,19 @@ class APIBinding:
 			code = self.number[2:-2]
 			if len(self.number) == 12:
 				if not self.number.startswith("+"):
-					code = self.number[3:-2]  		# may be +375123456789 
-	
-			elif len(self.number) == 13:			# so we need 1234567
+					code = self.number[3:-2]        # may be +375123456789
+
+			elif len(self.number) == 13:            # so we need 1234567
 				if self.number.startswith("+"):
 					code = self.number[4:-2]
 
-			values = { "act": "security_check",
-					   "al": "1",
-					   "al_page": "3",
-					   "code": code,
-					   "hash": Hash,
-					   "to": ""
-					  }
+			values = {"act": "security_check",
+			"al": "1",
+			"al_page": "3",
+			"code": code,
+			"hash": Hash,
+			"to": ""}
+
 			post = self.RIP.post("https://vk.com/login.php", values)
 			body, response = post
 			if response and not body.split("<!>")[4] == "4":
@@ -150,12 +154,11 @@ class APIBinding:
 
 	def confirmThisApp(self):
 		url = "https://oauth.vk.com/authorize"
-		values = { "display": "mobile",
-				   "scope": self.scope,
-				   "client_id": self.app_id,
-				   "response_type": "token",
-				   "redirect_uri": "https://oauth.vk.com/blank.html"
-				  }
+		values = {"display": "mobile",
+		"scope": self.scope,
+		"client_id": self.app_id,
+		"response_type": "token",
+		"redirect_uri": "https://oauth.vk.com/blank.html"}
 
 		token = None
 		get = self.RIP.get(url, values)
@@ -166,15 +169,17 @@ class APIBinding:
 			else:
 				PostTarget = webtools.getTagArg("form method=\"post\"", "action", body, "form")
 				if PostTarget:
-					post = self.RIP.post(PostTarget) # why no data?
+					post = self.RIP.post(PostTarget)    # why no data?
 					body, response = post
 					token = response.url.split("=")[1].split("&")[0]
 				else:
 					raise AuthError("Couldn't execute confirmThisApp()!")
 		self.token = token
 
+	def method(self, method, values=None):
+		if values is None:
+			values = {}
 
-	def method(self, method, values = {}):
 		url = "https://api.vk.com/method/%s" % method
 		values["access_token"] = self.token
 		values["v"] = "3.0"
@@ -187,25 +192,25 @@ class APIBinding:
 		self.last.append(time.time())
 		if len(self.last) > 2:
 			if (self.last.pop() - self.last.pop(0)) < 1.1:
-				time.sleep(0.3) # warn: it was 0.4 // does it matter?
+				time.sleep(0.3)     # warn: it was 0.4 // does it matter?
 
-		post = self.RIP.post(url, values)
+		body, response = self.RIP.post(url, values)
 
-		body, response = post
 		if body:
 			body = json.loads(body)
-# Debug:
-##		if method == "users.get":##("messages.get", "messages.send"):
-##			print "method %s with values %s" % (method, str(values))
-##			print "response for method %s: %s" % (method, str(body))
-		if body.has_key("response"):
+		# Debug:
+		##		if method == "users.get":##("messages.get", "messages.send"):
+		##			print "method %s with values %s" % (method, str(values))
+		##			print "response for method %s: %s" % (method, str(body))
+		if "response" in body:
 			return body["response"]
 
-		elif body.has_key("error"):
+		elif "error" in body:
 			error = body["error"]
 			eCode = error["error_code"]
-## TODO: Check this code
-			if eCode == 5: # invalid token
+			# TODO: Check this code
+			# TODO: use exception-based error processing (refactor)
+			if eCode == 5:  # invalid token
 				self.attempts += 1
 				if self.attempts < 3:
 					retry = self.retry()
@@ -214,36 +219,41 @@ class APIBinding:
 						return retry
 				else:
 					raise TokenError(error["error_msg"])
-			if eCode == 6: # too fast
+			if eCode == 6:  # too fast
 				time.sleep(3)
 				return self.method(method, values)
-			elif eCode == 5: # auth failed
+			elif eCode == 5:    # auth failed
 				raise VkApiError("Logged out")
 			if eCode == 7:
 				raise NotAllowed
 			elif eCode == 9:
 				return {}
-			if eCode == 14: # captcha
-				if error.has_key("captcha_sid"):
+			if eCode == 14:     # captcha
+				if "captcha_sid" in error:
 					self.captcha = {"sid": error["captcha_sid"], "img": error["captcha_img"]}
 					raise CaptchaNeeded
 			raise VkApiError(body["error"])
 
 	def retry(self):
-		if self.lastMethod: 
+		if self.lastMethod:
 			return self.method(*self.lastMethod)
+
 
 class VkApiError(Exception):
 	pass
 
+
 class AuthError(VkApiError):
 	pass
+
 
 class CaptchaNeeded(VkApiError):
 	pass
 
+
 class TokenError(VkApiError):
 	pass
+
 
 class NotAllowed(VkApiError):
 	pass
